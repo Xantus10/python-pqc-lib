549090fdd8231defc29084ad8490b61d97844cdf
xantus10 <jaroslav.zaba@post.cz>
2026-08-06 15:23:05 +0200

    Hints and bits functions

--- src/python_pqc_lib/dsa/mldsa/encoding.py
+++ src/python_pqc_lib/dsa/mldsa/encoding.py
@@ -220,3 +220,117 @@ def hintBitUnpack(b: bytes, k: int, omega: int) -> np.typing.NDArray[Any]:
   for i in range(ix, omega):
     if b[i] != 0: raise IndexError('Malformed bytes input for hint')
   return ret
+
+
+def centered_mod(x: int, q: int):
+  """
+  Compute a x % q with result in range -q/2...q/2
+
+  Args:
+    x (int): The number
+    q (int): The modulo
+
+  Returns:
+    The remainder centered around 0
+  """
+  res = x % q
+  if res > (q-1) // 2:
+    res -= q
+  return res
+
+def power2Round(r: int, d: int) -> tuple[int, int]:
+  """
+  Decompose an integer into two parts - multiplier and constant
+
+  Args:
+    r (int): The integer to decompose
+    d (int): Number of bits
+
+  Returns:
+    A multiplier and a constant so that r = m * 2**d + c
+  """
+  r_pos = r % Q
+  r0 = centered_mod(r_pos, 2**d)
+  return (r_pos - r0) // (2**d), r0
+
+def decompose(r: int, gamma2: int) -> tuple[int, int]:
+  """
+  Decompose an integer into two parts - multiplier and constant
+
+  Args:
+    r (int): The integer to decompose
+    gamma2 (int): The multiplier part
+
+  Returns:
+    A multiplier and a constant so that r = m * 2gamma + c
+  """
+  r_pos = r % Q
+  r0 = centered_mod(r_pos, 2 * gamma2)
+  if r_pos - r0 == Q - 1:
+    r1 = 0
+    r0 -= 1
+  else:
+    r1 = (r_pos - r0) // (2 * gamma2)
+  return r1, r0
+
+def highBits(r: int, gamma2: int) -> int:
+  """
+  Return the mult part of decompose
+
+  Args:
+    r (int): The integer to decompose
+    gamma2 (int): Rounding range
+
+  Returns:
+    The mult part of decompose
+  """
+  return decompose(r, gamma2)[0]
+
+def lowBits(r: int, gamma2: int) -> int:
+  """
+  Return the const part of decompose
+
+  Args:
+    r (int): The integer to decompose
+    gamma2 (int): Rounding range
+
+  Returns:
+    The const part of decompose
+  """
+  return decompose(r, gamma2)[1]
+
+
+def makeHint(to_add: int, base: int, gamma2: int) -> bool:
+  """
+  Find whether adding to_add will change the high bits of base
+
+  Args:
+    to_add (int): Number to add
+    base (int): Number whose high bits are examined
+    gamma2 (int): Rounding range
+
+  Returns:
+    True when the high bits change
+  """
+  base_high = highBits(base, gamma2)
+  res_high = highBits(base + to_add, gamma2)
+  return base_high != res_high
+
+def useHint(hint: bool, r: int, gamma2: int):
+  """
+  Return the high bits of r, adjusted based on hint
+
+  Args:
+    hint (bool): Result of makeHint
+    r (int): Number to act on
+    gamma2 (int): Rounding range
+
+  Returns:
+    Adjusted high bits of r
+  """
+  mod = (Q - 1) // (2 * gamma2)
+  r1, r0 = decompose(r, gamma2)
+  if hint and r0 > 0: return (r1 + 1) % mod
+  if hint and r0 <= 0: return (r1 - 1) % mod
+  return r1
+
