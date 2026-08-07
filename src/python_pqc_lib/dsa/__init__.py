e5d276db831fa521a074996185f0b7d79556e3cf
xantus10 <jaroslav.zaba@post.cz>
2026-08-07 12:24:02 +0200

    Added __all__ import

--- src/python_pqc_lib/dsa/__init__.py
+++ src/python_pqc_lib/dsa/__init__.py
@@ -1,2 +1,4 @@
 from .mldsa.mldsa import MLDSA
 from .mldsa.constants import MLDSA_44, MLDSA_65, MLDSA_87
+
+__all__ = ['MLDSA', 'MLDSA_44', 'MLDSA_65', 'MLDSA_87']
--- src/python_pqc_lib/kem/__init__.py
+++ src/python_pqc_lib/kem/__init__.py
@@ -1,3 +1,4 @@
 from .mlkem.mlkem import MLKEM
 from .mlkem.constants import MLKEM_512, MLKEM_768, MLKEM_1024
 
+__all__ = ['MLKEM', 'MLKEM_512', 'MLKEM_768', 'MLKEM_1024']
