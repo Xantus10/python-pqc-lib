2406ed4d78579edf0e7abc1d1f1356fab9a91be6
xantus10 <jaroslav.zaba@post.cz>
2026-08-07 12:21:25 +0200

    Sign and HashSign done

---
+++ src/python_pqc_lib/dsa/__init__.py
@@ -0,0 +1,2 @@
+from .mldsa.mldsa import MLDSA
+from .mldsa.constants import MLDSA_44, MLDSA_65, MLDSA_87
--- src/python_pqc_lib/dsa/mldsa/mldsa.py
+++ src/python_pqc_lib/dsa/mldsa/mldsa.py
@@ -4,21 +4,24 @@ Main ML DSA class
 import numpy as np
 
 from secrets import token_bytes
+from hashlib import sha256, sha512, shake_128
 
-from .constants import MLDSA_Parameters, Q
-from .encoding import power2RoundVec, simpleBitPack, bitPack
+from .constants import MLDSA_Parameters, Q, SIGN_BOUND
+from .encoding import power2RoundVec, simpleBitPack, bitPack, simpleBitUnpack, bitUnpack, highBits, lowBits, makeHint, hintBitPack
 from .mmatrix import MMatrix
 from .mvector import MVector
-from .random_helper import sampleInBall, sampleMatrixPol, sampleSmallPolynomial, h
+from .random_helper import sampleInBall, sampleMatrixPol, sampleSmallPolynomial, h, expandMask
 
-class MLKEM:
-  """Class representing an ML KEM state"""
+from typing import Literal
+
+class MLDSA:
+  """Class representing an ML DSA state"""
   def __init__(self, parameters: MLDSA_Parameters):
     """
-    Class representing an ML KEM state
+    Class representing an ML DSA state
 
     Args:
-      parameters (MLKEM_Parameters): Special list of parameters for ML KEM
+      parameters (MLDSA_Parameters): Special list of parameters for ML DSA
     """
     # Parameters
     self.d = parameters[0]
@@ -30,6 +33,7 @@ class MLKEM:
     self.l = parameters[6]
     self.eta = parameters[7]
     self.omega = parameters[8]
+    self.beta = self.tau * self.eta
     # Raw math objects
     self.__matrix: MMatrix = None
     # Keys
@@ -69,12 +73,15 @@ class MLKEM:
       uniq_n += 1
     return MVector.from_coefficients(vec_coefficients)
 
-  def __innerKeyGen(self, seed: bytes):
+  def __innerKeyGen(self, seed: bytes) -> tuple[bytes, bytes]:
     """
     Handle key generation from seed
 
     Args:
       seed (bytes): The seed to sample from
+
+    Returns:
+      The public key and secret key
     """
     tmp_rand = h(seed + self.k.to_bytes() + self.l.to_bytes()).digest(128)
     matrix_seed, secret_seed, random_k = tmp_rand[0:32], tmp_rand[32:96], tmp_rand[96:128]
@@ -83,11 +90,136 @@ class MLKEM:
     secret_vec2 = self.generateSecretVector(secret_seed, self.l, self.eta, self.k)
     vec_t = (self.__matrix @ secret_vec1.NTT()).invNTT() + secret_vec2
     t1, t0 = power2RoundVec(vec_t.arr, self.d)
-    self.public_key = matrix_seed + b''.join([simpleBitPack(p, 2**((Q-1).bit_length() - self.d)) for p in t1])
-    pk_hash = h(self.public_key).digest(64)
-    self.__secret_key = matrix_seed + random_k + pk_hash
-    self.__secret_key += b''.join([bitPack(p, -self.eta, self.eta) for p in secret_vec1])
-    self.__secret_key += b''.join([bitPack(p, -self.eta, self.eta) for p in secret_vec2])
-    self.__secret_key += b''.join([bitPack(p, -(2**(self.d-1)) + 1, 2**(self.d-1)) for p in t0])
+    pk = matrix_seed + b''.join([simpleBitPack(p, (2**((Q-1).bit_length() - self.d)) - 1) for p in t1])
+    pk_hash = h(pk).digest(64)
+    sk = matrix_seed + random_k + pk_hash
+    sk += b''.join([bitPack(p, -self.eta, self.eta) for p in secret_vec1.arr])
+    sk += b''.join([bitPack(p, -self.eta, self.eta) for p in secret_vec2.arr])
+    sk += b''.join([bitPack(p, -(2**(self.d-1)) + 1, 2**(self.d-1)) for p in t0])
+    return pk, sk
 
+  def KeyGen(self):
+    """
+    Generate keys for ML DSA
+
+    Keys are stored internally
+    """
+    random_seed = token_bytes(32)
+    self.public_key, self.__secret_key = self.__innerKeyGen(random_seed)
+    print(len(self.public_key), len(self.__secret_key))
+
+
+  def __innerSign(self, secret_key: bytes, message: bytes, random_seed: bytes) -> bytes:
+    """
+    Perform the math for signing
+
+    Args:
+      secret_key (bytes): The secret key to sign with
+      message_bits (bytes): The message to sign
+      random_seed (bytes): The random seed to use
+
+    Returns:
+      The byte form of the signature
+
+    Raises:
+      RuntimeError: The while loop has exceeded its bound
+    """
+    matrix_seed = secret_key[0:32]
+    random_k = secret_key[32:64]
+    pk_hash = secret_key[64:128]
+    svec_step_size = 32 * ((2 * self.eta).bit_length())
+    svec1_end = 128 + svec_step_size * self.l
+    secret_vec1 = MVector.from_coefficients(
+      [bitUnpack(secret_key[i:i+svec_step_size], -self.eta, self.eta) for i in range(128, svec1_end, svec_step_size)]
+    ).NTT()
+    svec2_end = svec1_end + svec_step_size * self.k
+    secret_vec2 = MVector.from_coefficients(
+      [bitUnpack(secret_key[i:i+svec_step_size], -self.eta, self.eta) for i in range(svec1_end, svec2_end, svec_step_size)]
+    ).NTT()
+    tvec_step_size = 32 * self.d
+    t0 = MVector.from_coefficients(
+      [bitUnpack(secret_key[i:i+svec_step_size], -(2**(self.d-1)) + 1, 2**(self.d-1)) for i in range(svec2_end, svec2_end + tvec_step_size * self.k, tvec_step_size)]
+    ).NTT()
+
+    self.__matrix = self.generateMatrix(matrix_seed)
+    mi = h(pk_hash + message).digest(64)
+    private_seed = h(random_k + random_seed + mi).digest(64)
+    z, hint = None, None
+    counter = 0
+    while z is None or hint is None and counter:
+      y = MVector(expandMask(private_seed, self.gamma1, self.l, counter))
+      w = (self.__matrix @ y.NTT()).invNTT()
+      w1 = MVector.from_coefficients([[highBits(w.arr[i, j], self.gamma2) for j in range(256)] for i in range(len(w.arr))])
+      commit_hash = h(mi + b''.join([simpleBitPack(p, ((Q-1) // (2 * self.gamma2)) - 1) for p in w1.arr])).digest(self.lambd // 4)
+      chall = sampleInBall(commit_hash, self.tau)
+      chall_ntt = chall.copy()
+      MVector.simpleNTT(chall_ntt)
+      c_s1 = (secret_vec1 * chall_ntt).invNTT()
+      c_s2 = (secret_vec2 * chall_ntt).invNTT()
+      z = y + c_s1
+      r_vec = w - c_s2
+      r0 = MVector.from_coefficients([[lowBits(r_vec.arr[i, j], self.gamma2) for j in range(256)] for i in range(len(r_vec.arr))])
+      if z.infinityNorm() >= self.gamma1 - self.beta or r0.infinityNorm() >= self.gamma2 - self.beta: z, hint = None, None
+      else:
+        c_t0 = (t0 * chall_ntt).invNTT()
+        base = w - c_s2 + c_t0
+        hint = np.array([[makeHint(-c_t0.arr[i, j], base.arr[i, j], self.gamma2) for j in range(256)] for i in range(len(c_t0.arr))], dtype=np.int8)
+        if c_t0.infinityNorm() >= self.gamma2 or np.sum(hint) > self.omega: z, hint = None, None
+      counter += 1
+      if counter > SIGN_BOUND: RuntimeError('ML-DSA: Sign entered infinite while loop')
+    sig = commit_hash
+    sig += b''.join([bitPack(p, -self.gamma1 + 1, self.gamma1) for p in z.centered_modQ().arr])
+    sig += hintBitPack(hint, self.omega)
+    return sig
+
+  def Sign(self, message: bytes, context: bytes) -> bytes:
+    """
+    Sign a message and a context string
+
+    Args:
+      message (bytes): The message to sign
+      context (bytes): Context string of max length 255 bytes
+
+    Returns:
+      The signature
+
+    Raises:
+      ValueError: Context is longer than 255 bytes
+    """
+    if len(context) > 255: raise ValueError('Context is too long')
+    random_seed = token_bytes(32)
+    updated_message = b'\x00' + len(context).to_bytes() + context + message
+    return self.__innerSign(self.__secret_key, updated_message, random_seed)
+
+  def HashSign(self, message: bytes, context: bytes, hash_alg: Literal['sha256'] | Literal['sha512'] | Literal['shake128']):
+    """
+    Sign a message digest and a context string
+
+    Args:
+      message (bytes): The message to sign
+      context (bytes): Context string of max length 255 bytes
+      hash (sha256 | sha512 | shake128): Which hash algorithm to use
+
+    Returns:
+      The signature
+
+    Raises:
+      ValueError: Context is longer than 255 bytes or Invalid hash alg was provided
+    """
+    if len(context) > 255: raise ValueError('Context is too long')
+    random_seed = token_bytes(32)
+    match hash_alg:
+      case 'sha256':
+        oid = b'\x06\x09\x60\x86\x48\x01\x65\x03\x04\x02\x01'
+        message_hash = sha256(message).digest()
+      case 'sha512':
+        oid = b'\x06\x09\x60\x86\x48\x01\x65\x03\x04\x02\x03'
+        message_hash = sha512(message).digest()
+      case 'shake128':
+        oid = b'\x06\x09\x60\x86\x48\x01\x65\x03\x04\x02\x0b'
+        message_hash = shake_128(message).digest(32)
+      case _:
+        raise ValueError(f'Unsupported hash algorithm \'{hash_alg}\'')
+    updated_message = b'\x01' + len(context).to_bytes() + context + oid + message_hash
+    return self.__innerSign(self.__secret_key, updated_message, random_seed)
 
