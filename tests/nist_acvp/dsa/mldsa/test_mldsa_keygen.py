24e0d217aa40b03ef1a36a878aa488936b012f97
xantus10 <jaroslav.zaba@post.cz>
2026-08-13 11:35:57 +0200

    ML DSA Sign NIST tests

--- tests/nist_acvp/dsa/mldsa/test_mldsa_keygen.py
+++ tests/nist_acvp/dsa/mldsa/test_mldsa_keygen.py
@@ -1,6 +1,7 @@
 import pytest
 import json
 from pathlib import Path
+from hashlib import sha256
 
 from python_pqc_lib.dsa import *
 
@@ -55,4 +56,54 @@ def test_mldsa_87_keygen(mldsa_87_inst, seed, pk, sk):
   kg_pk, kg_sk = mldsa_87_inst._deterministicKeyGen(seed)
   assert kg_pk == pk
   assert kg_sk == sk
+
+
+def load_sign_json(fn: str):
+  with open(fn, 'r') as f:
+    content = json.load(f)
+  return [
+    (True, bytes.fromhex(v['seed']), bytes.fromhex(v['message']), bytes.fromhex(v['context']), bytes.fromhex(v['signature'])) for v in content['seed']
+  ] + [
+    (False, bytes.fromhex(v['sk']), bytes.fromhex(v['message']), bytes.fromhex(v['context']), bytes.fromhex(v['signature'])) for v in content['key']
+  ]
+
+def long_id_func(val):
+  return val[0:64] if isinstance(val, bytes) else str(val)
+
+data_44_sig = load_sign_json(cur_dir / 'nist_test_sign_44.json')
+
+@pytest.mark.parametrize(['isKeyGen', 'secret', 'msg', 'ctx', 'sig'], data_44_sig, ids=long_id_func)
+def test_mldsa_44_sign(mldsa_44_inst, isKeyGen, secret, msg, ctx, sig):
+  if isKeyGen:
+    _, sk = mldsa_44_inst._deterministicKeyGen(secret)
+    mldsa_44_inst._testSetSecretKey(sk)
+  else:
+    mldsa_44_inst._testSetSecretKey(secret)
+  gen_sig = mldsa_44_inst._deterministicSign(msg, ctx, b'\x00'*32)
+  assert gen_sig == sig
+
+data_65_sig = load_sign_json(cur_dir / 'nist_test_sign_65.json')
+
+@pytest.mark.parametrize(['isKeyGen', 'secret', 'msg', 'ctx', 'sig'], data_65_sig, ids=long_id_func)
+def test_mldsa_65_sign(mldsa_65_inst, isKeyGen, secret, msg, ctx, sig):
+  if isKeyGen:
+    _, sk = mldsa_65_inst._deterministicKeyGen(secret)
+    mldsa_65_inst._testSetSecretKey(sk)
+  else:
+    mldsa_65_inst._testSetSecretKey(secret)
+  gen_sig = mldsa_65_inst._deterministicSign(msg, ctx, b'\x00'*32)
+  assert gen_sig == sig
+
+
+data_87_sig = load_sign_json(cur_dir / 'nist_test_sign_87.json')
+
+@pytest.mark.parametrize(['isKeyGen', 'secret', 'msg', 'ctx', 'sig'], data_87_sig, ids=long_id_func)
+def test_mldsa_87_sign(mldsa_87_inst, isKeyGen, secret, msg, ctx, sig):
+  if isKeyGen:
+    _, sk = mldsa_87_inst._deterministicKeyGen(secret)
+    mldsa_87_inst._testSetSecretKey(sk)
+  else:
+    mldsa_87_inst._testSetSecretKey(secret)
+  gen_sig = mldsa_87_inst._deterministicSign(msg, ctx, b'\x00'*32)
+  assert gen_sig == sig
 
